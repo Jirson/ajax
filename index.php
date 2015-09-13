0eed8d9593949bfe6f0ce694f76aab17ebe8302f
Jirson Tavera <JirsonTavera@hotmail.com>
2015-09-13 08:01:03 -0500

    File index.php ajax example

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es-CO" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>añañay Ajax</title>
+        <!-- <script src=""></script> -->
+    </head>
+    <body>
+        <form action="" method="POST" id="formAjax">
+            <input type="text" name="numberOne" placeholder="Número uno">
+            <input type="text" name="numberTwo" placeholder="Número dos">
+            <input type="submit" name="enviar" value="Enviar">
+        </form>
+    </body>
+</html>
